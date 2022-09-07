97be53e0165798284362a82aece1abf1fb5f573c
Nik Tysiachnyi <nikita.tysiachnyi@eduki.com>
2022-09-07 03:15:03 +0300

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import ApplicationTopBar from './components/AppBar/AppBar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ApplicationTopBar />
+    </div>
+  );
 }
 
 export default App;
